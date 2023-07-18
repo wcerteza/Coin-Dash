19b05f27cc134220ff0e9e11e18c96363ba83e5e
Will Certeza <wcerteza@gmail.com>
2023-07-18 12:24:14 -0500

    added more fields for the portfolio page

--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import { DeleteCoinFromPortfolio } from '../services/PortfolioServices'
+import { Profiler } from 'react'
 
 const Portfolio = ({ user, portfolio, setLookForPortfolio }) => {
   let navigate = useNavigate()
@@ -17,8 +18,11 @@ const Portfolio = ({ user, portfolio, setLookForPortfolio }) => {
     <div className="portfolio-page">
       <h1>Your Coins</h1>
       {portfolio.coins.map((coin, idx) => (
-        <div key={coin._id}>
+        <div key={coin._id} className="portfolio-container">
+          <img src={coin.coinId.image} alt="" />
+          <p>{coin.coinId.market_cap_rank}.</p>
           <p>{coin.coinId.name}</p>
+          <p>({coin.coinId.symbol})</p>
           <button onClick={() => handleDeleteCoin(idx)}>delete</button>
         </div>
       ))}
