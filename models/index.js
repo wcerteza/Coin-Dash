2f76492cf629f8533c8538d724df9e13276e969c
Will Certeza <wcerteza@gmail.com>
2023-07-18 18:37:09 -0500

    glenn bananna

--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 const userSchema = require('./User')
-const coinSchema = require('./Coin')
+
 const portfolioSchema = require('./Portfolio')
 const chatSchema = require('./Chat')
+const coinSchema = require('./Coin')
 
 const User = mongoose.model('User', userSchema)
 const Coin = mongoose.model('Coin', coinSchema)
